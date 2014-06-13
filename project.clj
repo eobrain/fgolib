a8f2d5f9e20fd5605cab7814825ce31ae6f2fe2d
Eamonn O'Brien-Strain <eob@well.com>
2014-06-12 22:50:11 -0700

    release 0.2.3

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.eamonn.funcgo/fgolib "0.2.2-SNAPSHOT"
+(defproject org.eamonn.funcgo/fgolib "0.2.3"
   :description "Library that mimics the standard Go library"
   :url "http://funcgo.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.eamonn.funcgo/funcgo-lein-plugin "0.2.4-APPENGINE"]
+                 [org.eamonn.funcgo/funcgo-lein-plugin "0.2.5"]
                  [midje "1.5.1" :scope "test"]]
   :plugins [[lein-midje "3.1.1"]
-            [org.eamonn.funcgo/funcgo-lein-plugin "0.2.4-APPENGINE"]])
+            [org.eamonn.funcgo/funcgo-lein-plugin "0.2.5"]])
